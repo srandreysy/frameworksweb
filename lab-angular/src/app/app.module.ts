8af1fc67a8d30cc45c7bae60e9b46c817e549de4
srandreysy <73713624+srandreysy@users.noreply.github.com>
2020-11-19 04:39:25 -0300

    Add files via upload

---
+++ lab-angular/src/app/app.module.ts
@@ -0,0 +1,24 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { RodapeComponent } from './rodape/rodape.component';
+import { ContadorComponent } from './contador/contador.component';
+import { CardComponent } from './card/card.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    RodapeComponent,
+    ContadorComponent,
+    CardComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
